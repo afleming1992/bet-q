14fb4def471249517be339e2ab8b6f2c761af708
Andrew Fleming <ajfleming1992@gmail.com>
2018-06-09 00:03:09 +0100

    Changed package.json to move to a hot reloading setup to make dev easier and quicker

--- service/src/controllers/GameController.ts
+++ service/src/controllers/GameController.ts
@@ -3,6 +3,7 @@ import { Game, GameStatus } from './../models/Game';
 import { IGameDB } from './../services/db/IGameDB';
 import { GameDBDynamo } from './../services/db/GameDBDynamo';
 import { AppError, AppErrorCode } from './../models/AppError';
+
 import { Router, Request, Response, NextFunction } from 'express';
 
 export class GameController {
@@ -32,19 +33,33 @@ export class GameController {
                 });
     }
 
-    public getGame(req: Request, res: Response) {
+    public async getGame(req: Request, res: Response) {
         const id = req.params.id;
         if (id.length !== 36) {
             res.status(400);
             res.send(new AppError(AppErrorCode.BadRequest, 'Game ID is not a valid UUID'));
         } else {
-            this.db.getGame(id)
-            .then((game: Game) => {
+            try {
+                const game = await this.getGameInternal(id);
                 res.send(game);
-            })
-            .catch((error: AppError) => {
+            } catch (error) {
                 res.send(error);
-            });
+            }
         }
+    }
+
+    private getGameInternal(id: string) {
+        return this.db.getGame(id)
+        .then((game: Game) => {
+            return game;
+        })
+        .catch((error: AppError) => {
+            throw error;
+        });
+    }
+
+    public getCategory(req: Request, res: Response) {
+        const id = req.params.id;
+        const test = 'Hell';
     }
 }
